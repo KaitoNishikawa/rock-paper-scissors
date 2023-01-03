887d1e3d196fae9126cd0088d281a353efecf8d6
Kaito Nishikawa <kai.nishikawa808@gmail.com>
2023-01-02 16:55:37 -0800

    onscreen results

--- app.js
+++ app.js
@@ -1,40 +1,63 @@
-function janken(selection){
-    var computer = Math.floor(Math.random() * 3);
+let btns = document.querySelectorAll('button');
+var result = document.getElementById('result');
+var computerSelection = ['Rock', 'Paper', 'Scissors'];
+var counter = 0;
+var computer;
 
-    console.log(computer);
+for(let i = 0; i < btns.length; i++){
+    btns[i].addEventListener('click', () => {
+        janken(i);
+    });
+}
+
+
+
+function janken(selection){
+    computer = Math.floor(Math.random() * 3);
+
+    console.log("computer " + computer);
+    console.log("player " + selection);
+
+    result.innerHTML = 'You did: ' + computerSelection[selection] + "<br />" + 'Computer did: ' + computerSelection[computer];    
 
     if(selection == computer){
         console.log("tie");
-        
+        result.innerHTML = result.innerHTML + '<br />Tie';        
     }
 
     else if(selection == 0){
         if(computer == 1){
             console.log("lose");
+            result.innerHTML = result.innerHTML + '<br />You Lose';
         }        
 
         else{
             console.log("win");
+            result.innerHTML = result.innerHTML + '<br />You Win';
         }
     }
 
     else if(selection == 1){
         if(computer == 2){
             console.log("lose");
+            result.innerHTML = result.innerHTML + '<br />You Lose';
         }        
 
         else{
             console.log("win");
+            result.innerHTML = result.innerHTML + '<br />You Win';
         }
     }
 
     else{
         if(computer == 0){
             console.log("lose");
+            result.innerHTML = result.innerHTML + '<br />You Lose';
         }
 
         else{
             console.log("win");
+            result.innerHTML = result.innerHTML + '<br />You Win';
         }
     }
 
